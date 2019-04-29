04b0dbb6859886c60cf77ad540cc0acb68559930
John Hayes <johnnyhayes7@gmail.com>
2019-04-29 15:29:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
     "#{Math.sqrt(params[:number].to_i).to_s}"
   end
   
-  get 'say/:number/:phrase' do
+  get '/say/:number/:phrase' do
     params[:number].to_i.times
     puts "#{params[:phrase]}"
   end
