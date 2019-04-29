46ca493517e87178383306c0318accab4407f957
John Hayes <johnnyhayes7@gmail.com>
2019-04-29 16:57:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     result = " "
     i = 0 
-      until i > params[:number]
+      until i > params[:number].to_i
       result += params[:phrase]
       i +=1 
       end
